ff4c2d55dd677d4787240ac7f568876da280f0e6
Isabel <isabel.gogov1@libero.it>
2020-10-22 21:11:52 +0200

    controllo email pate js finito

--- invitati/js/script.js
+++ invitati/js/script.js
@@ -4,34 +4,36 @@
 
 var inputEmail = document.getElementById("email");
 var bvai = document.getElementById("vai");
+var outputEmail = "";
+var confronto = "false";
+var risultato;
 
 // creazione lista email degli invitati
-var invitati = ["max@gmail.com","pippo@gmail.com","john@gmail.com","doe@gmail.com","anonymous@gmail.com"];
+var invitati = ["max@gmail.com", "pippo@gmail.com", "john@gmail.com", "doe@gmail.com", "anonymous@gmail.com"];
 
 // bottone controllo email
 bvai.addEventListener("click",
   function() {
     // ciclo per traversare le email
     for (var i = 0; i < invitati.length; i++) {
-      invitati[i]
-      console.log(invitati[i]);
+      outputEmail = outputEmail + invitati[i];
+
+      // condizioni confronto
+      if (inputEmail.value === invitati[i]) {
+        confronto = "true";
+      }
+      console.log(inputEmail.value);
     }
-//
-//
-//     // condizioni contronto tra email
-//     if (dadUt < dadPc) {
-//       risultato = " mi dispiace hai perso :(";
-//
-//     } else if (dadUt > dadPc) {
-//       risultato = " congratulazioni hai vinto :D";
-//
-//     } else {
-//       risultato = " hai pareggiato, ritenta";
-//     }
-//     console.log("risultato utente: ",risultato);
-//
-//     // OUTPUT
-//     document.getElementById("outputDado").innerHTML = nome.value + ": " + dadUt + "<br>" + "pc: " + dadPc;
-//     document.getElementById("output").innerHTML = nome.value + "," + risultato;
-//
+
+    // condizione risultato
+    if ( confronto === "true" ){
+      risultato = "benvenuto, sei stato invitato alla festa"
+
+    } else {
+      risultato = "mi disp, non ci sei"
+    }
+    console.log(risultato);
+
+  // OUTPUT
+  document.getElementById("outputMex").innerHTML = risultato;
 } );
